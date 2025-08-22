a773ac1c4dfb494d148462e306e6e7aa9597baaa
SatsuyaSystems <projectgremory.api@yahoo.com>
2025-08-22 21:59:27 +0200

    guild tag in chat messages

--- src/main/java/de/satsuya/elysiumCore/events/ChatEvent.java
+++ src/main/java/de/satsuya/elysiumCore/events/ChatEvent.java
@@ -1,5 +1,7 @@
 package de.satsuya.elysiumCore.events;
 
+import de.satsuya.elysiumCore.ElysiumCore;
+import de.satsuya.elysiumCore.manager.GuildManager;
 import de.satsuya.elysiumCore.utils.ConfigLoader;
 import de.satsuya.elysiumCore.utils.ElysiumLogger;
 import org.bukkit.Bukkit;
@@ -9,6 +11,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatEvent implements Listener {
+    private final GuildManager guildManager;
+
+    public ChatEvent() {
+        this.guildManager = ElysiumCore.getGuildManager();
+    }
+
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
         Player sender = event.getPlayer();
@@ -23,6 +31,10 @@ public class ChatEvent implements Listener {
             if (sender.getWorld().equals(recipient.getWorld())) {
                 double distance = sender.getLocation().distance(recipient.getLocation());
                 if (distance <= ConfigLoader.configData.getInt("chatRadius") || recipient.equals(sender)) {
+                    if (guildManager.isPlayerInGuild(sender.getUniqueId())) {
+                        recipient.sendMessage("["+ guildManager.getPlayerGuild(sender.getUniqueId()) +"] <" + sender.getDisplayName() + "> " + message);
+                        return;
+                    }
                     recipient.sendMessage("<" + sender.getDisplayName() + "> " + message);
                 }
             }
